86ccd0ee9daa39ceb8039503a7ade074758057e7
WillOliver7 <guilhermejno@gmail.com>
2026-07-29 11:55:53 -0300

    Before/after context hooks usage

--- rspec_course/spec/before_and_after_spec.rb
+++ rspec_course/spec/before_and_after_spec.rb
@@ -1,10 +1,19 @@
 RSpec.describe 'before and after hooks' do
+  before(:context) do
+    puts 'Before context | This runs before the entire context we are in, in this case the entire describe block'
+  end
+
+  #This runs after the entire context we're in, in this case the entire describe block
+  after(:context) do
+    puts 'After context | This runs after the entire context we are in, in this case the entire describe block'
+  end
+
   before(:example) do
-    puts 'Before example'
+    puts 'Before example | This runs before each example in the context we are in, in this case the entire describe block'
   end
 
   after(:example) do
-    puts 'After example'
+    puts 'After example | This runs after each example in the context we are in, in this case the entire describe block'
   end
 
   it 'is just a random example' do
